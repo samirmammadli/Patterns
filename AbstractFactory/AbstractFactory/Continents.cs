73bf41c4f99499e3241a42e227d3693e8f0959e2
Samir <Samir4ik86@gmail.com>
2018-02-09 12:07:24 +0400

    1

---
+++ AbstractFactory/AbstractFactory/Continents.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    interface IContinent
+    {
+        Carnivore BuildCarnivore();
+        Herbivore BuildHerbivore();
+    }
+
+    class Africa : IContinent
+    {
+        public Carnivore BuildCarnivore()
+        {
+            return new Leon();
+        }
+
+        public Herbivore BuildHerbivore()
+        {
+            return new Wildebeest();
+        }
+    }
+
+    class NorthAmerica : IContinent
+    {
+        public Carnivore BuildCarnivore()
+        {
+            return new Wolf();
+        }
+
+        public Herbivore BuildHerbivore()
+        {
+            return new Bison();
+        }
+    }
+
+    class Eurasia : IContinent
+    {
+        public Carnivore BuildCarnivore()
+        {
+            return new Tiger();
+        }
+
+        public Herbivore BuildHerbivore()
+        {
+            return new Elk();
+        }
+    }
+}
--- AbstractFactory/AbstractFactory/Program.cs
+++ AbstractFactory/AbstractFactory/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,103 +6,33 @@ using System.Threading.Tasks;
 namespace AbstractFactory
 {
 
-    abstract class Herbivore
-    {
-        public int Weight { get; set; }
-        public bool Life { get; set; }
-
-        public void EatGrass()
-        {
-            Weight += 10;
-        }
-    }
-
-    abstract class Carnivore
-    {
-        public int Strenght { get; set; }
-
-        public void EatHerbivore(Herbivore animal)
-        {
-            if (animal.Weight > Strenght)
-                Strenght -= 10;
-            else
-                Strenght += 10;
-        }
-    }
-
-    class Wildebeest : Herbivore
-    {
-        public Wildebeest()
-        {
-            Life = true;
-            Weight = 120;
-        }
-    }
-
-    class Bison : Herbivore
-    {
-        public Bison()
-        {
-            Life = true;
-            Weight = 300;
-        }
-    }
-
-    class Leon : Carnivore
-    {
-        public Leon()
-        {
-            Strenght = 300;
-        }
-    }
-
-    class Wolf : Carnivore
-    {
-        public Wolf()
-        {
-            Strenght = 150;
-        }
-    }
-
-    interface IContinent
-    {
-        Carnivore BuildCarnivore();
-        Herbivore BuildHerbivore();
-    }
-
-    class Africa : IContinent
-    {
-        public Carnivore BuildCarnivore()
-        {
-            return new Leon();
-        }
-
-        public Herbivore BuildHerbivore()
-        {
-            return new Wildebeest();
-        }
-    }
-
-    class NorthAmerica : IContinent
-    {
-        public Carnivore BuildCarnivore()
-        {
-            return new Wolf();
-        }
-
-        public Herbivore BuildHerbivore()
-        {
-            return new Bison();
-        }
-    }
-
-
-
     class Program
     {
         static void Main(string[] args)
         {
-           
+            var animalWorld = new AnimalWorld();
+
+            animalWorld.Continent = new Africa();
+            animalWorld.Addanimals();
+
+            animalWorld.MealsHerbivore();
+            animalWorld.NutritionCarnivores();
+
+            Console.WriteLine();
+
+            animalWorld.Continent = new NorthAmerica();
+            animalWorld.Addanimals();
+
+            animalWorld.MealsHerbivore();
+            animalWorld.NutritionCarnivores();
+
+            Console.WriteLine();
+
+            animalWorld.Continent = new Eurasia();
+            animalWorld.Addanimals();
+
+            animalWorld.MealsHerbivore();
+            animalWorld.NutritionCarnivores();
         }
     }
 }
