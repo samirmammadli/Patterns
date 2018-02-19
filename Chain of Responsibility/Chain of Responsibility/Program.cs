ad566567ec4523981d3a8266af0e8f97ee02b0fb
samir4ik86 <samir4ik86@gmail.com>
2018-02-19 21:32:06 +0400

    1

--- Chain of Responsibility/Chain of Responsibility/Program.cs
+++ Chain of Responsibility/Chain of Responsibility/Program.cs
@@ -6,6 +6,41 @@ using System.Threading.Tasks;
 
 namespace Chain_of_Responsibility
 {
+    abstract class ExceptionHandler
+    {
+        protected ExceptionHandler nextHandler;
+        protected MyException Exception;
+        abstract public void ErrorHandling(MyException ex);
+        public ExceptionHandler(ExceptionHandler handler = null) { nextHandler = handler; }
+    }
+
+    class NormalExceptionHandler : ExceptionHandler
+    {
+        public NormalExceptionHandler(ExceptionHandler handler) : base(handler) { }
+        public override void ErrorHandling(MyException ex)
+        {
+            
+        }
+    }
+
+    class CriticalExceptionHandler : ExceptionHandler
+    {
+        public CriticalExceptionHandler(ExceptionHandler handler) : base(handler) { }
+        public override void ErrorHandling(MyException ex)
+        {
+
+        }
+    }
+
+    enum ExceptionType { Normal, Critical, Fatal }
+    class MyException : Exception
+    {
+        public ExceptionType Type { get; private set; }
+        public MyException(string message, ExceptionType type = ExceptionType.Normal) : base(message)
+        {
+            Type = type;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
