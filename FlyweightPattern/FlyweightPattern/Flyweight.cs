30bb3345ce4a5263f36f9f21971b15bb1bb8a087
Samir <Samir4ik86@gmail.com>
2018-02-19 10:52:05 +0400

    1

--- FlyweightPattern/FlyweightPattern/Flyweight.cs
+++ FlyweightPattern/FlyweightPattern/Flyweight.cs
@@ -13,7 +13,7 @@ namespace FlyweightPattern
         Sea
     }
 
-    static class ImageLoader
+    static class ImageDownloader
     {
         static public Bitmap DownloadImage(string url)
         {
@@ -45,9 +45,9 @@ namespace FlyweightPattern
 
         private void LoadImages()
         {
-            Images.Add(Pictures.Sea, ImageLoader.DownloadImage("https://images.pexels.com/photos/37403/bora-bora-french-polynesia-sunset-ocean.jpg?h=350&auto=compress&cs=tinysrgb"));
-            Images.Add(Pictures.Forest, ImageLoader.DownloadImage("https://images.pexels.com/photos/240040/pexels-photo-240040.jpeg?h=350&auto=compress&cs=tinysrgb"));
-            Images.Add(Pictures.City, ImageLoader.DownloadImage("https://images.pexels.com/photos/373912/pexels-photo-373912.jpeg?h=350&auto=compress&cs=tinysrgb"));
+            Images.Add(Pictures.Sea, ImageDownloader.DownloadImage("https://images.pexels.com/photos/37403/bora-bora-french-polynesia-sunset-ocean.jpg?h=350&auto=compress&cs=tinysrgb"));
+            Images.Add(Pictures.Forest, ImageDownloader.DownloadImage("https://images.pexels.com/photos/240040/pexels-photo-240040.jpeg?h=350&auto=compress&cs=tinysrgb"));
+            Images.Add(Pictures.City, ImageDownloader.DownloadImage("https://images.pexels.com/photos/373912/pexels-photo-373912.jpeg?h=350&auto=compress&cs=tinysrgb"));
         }
     }
 }
