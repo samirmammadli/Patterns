760fdf6c991ac0446d3897de74cd196547ce2864
Samir <Samir4ik86@gmail.com>
2018-02-20 17:35:19 +0400

    1

--- ComandPattern/ComandPattern/Form1.Designer.cs
+++ ComandPattern/ComandPattern/Form1.Designer.cs
@@ -84,8 +84,8 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(160, 124);
             this.button1.TabIndex = 0;
-            this.button1.Text = "O";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
--- ComandPattern/ComandPattern/Form1.cs
+++ ComandPattern/ComandPattern/Form1.cs
@@ -12,9 +12,59 @@ namespace ComandPattern
 {
     public partial class Form1 : Form
     {
+        char[,] field;
         public Form1()
         {
+            field = new char[3,3] {
+                { 'o', 'a', 'o' },
+                { 'a', 'o', 'x' },
+                { 'o', 'i', 'o'} };
             InitializeComponent();
+            Metod();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "" || button1.Text == "O")
+                button1.Text = "X";
+            else if (button1.Text == "X")
+                button1.Text = "O";
+        }
+
+        private char isMatch(List<char> list)
+        {
+            var a = list.GroupBy(x => x).OrderByDescending(x => x.Count()).FirstOrDefault();
+            if(a != null && a.Count() == 3)
+                Console.WriteLine(a.Key);
+            return ' ';
+        }
+
+        private char Metod()
+        {
+            List<char> horizontal = new List<char>();
+            List<char> verticale = new List<char>();
+            List<char> diagonale1 = new List<char>();
+            List<char> diagonale2 = new List<char>();
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                horizontal.Clear();
+                verticale.Clear();
+                char result;
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    horizontal.Add(field[i, j]);
+                    if ((result = isMatch(horizontal)) != ' ') return result;
+
+                    verticale.Add(field[j, i]);
+                    if ((result = isMatch(verticale)) != ' ') return result;
+                }
+                diagonale1.Add(field[i, i]);
+                if ((result = isMatch(diagonale1)) != ' ') return result;
+
+                diagonale2.Add(field[i, field.GetLength(0) - 1 - i]);
+                if ((result = isMatch(diagonale2)) != ' ') return result;
+            }
+            return ' ';
         }
     }
 }
