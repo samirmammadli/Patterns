200d14047e06b94f80be62f5b9d752b481d30bc6
Samir <Samir4ik86@gmail.com>
2018-02-20 12:11:38 +0400

    1

--- ComandPattern/ComandPattern/Form1.Designer.cs
+++ ComandPattern/ComandPattern/Form1.Designer.cs
@@ -28,12 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tlpButtons = new System.Windows.Forms.TableLayoutPanel();
+            this.pnlField = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.tlpButtons.SuspendLayout();
+            this.pnlField.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpButtons
+            // 
+            this.tlpButtons.ColumnCount = 3;
+            this.tlpButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tlpButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tlpButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tlpButtons.Controls.Add(this.button9, 2, 2);
+            this.tlpButtons.Controls.Add(this.button8, 1, 2);
+            this.tlpButtons.Controls.Add(this.button7, 0, 2);
+            this.tlpButtons.Controls.Add(this.button6, 2, 1);
+            this.tlpButtons.Controls.Add(this.button5, 1, 1);
+            this.tlpButtons.Controls.Add(this.button4, 0, 1);
+            this.tlpButtons.Controls.Add(this.button3, 2, 0);
+            this.tlpButtons.Controls.Add(this.button2, 1, 0);
+            this.tlpButtons.Controls.Add(this.button1, 0, 0);
+            this.tlpButtons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpButtons.Location = new System.Drawing.Point(0, 0);
+            this.tlpButtons.Name = "tlpButtons";
+            this.tlpButtons.RowCount = 3;
+            this.tlpButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tlpButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tlpButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tlpButtons.Size = new System.Drawing.Size(499, 391);
+            this.tlpButtons.TabIndex = 0;
+            // 
+            // pnlField
+            // 
+            this.pnlField.Controls.Add(this.tlpButtons);
+            this.pnlField.Location = new System.Drawing.Point(12, 36);
+            this.pnlField.Name = "pnlField";
+            this.pnlField.Size = new System.Drawing.Size(499, 391);
+            this.pnlField.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 124);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "O";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(169, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 124);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(335, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 124);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(3, 133);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 124);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "X";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(169, 133);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(160, 124);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "X";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(335, 133);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(161, 124);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "X";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(3, 263);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(160, 125);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "X";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Location = new System.Drawing.Point(169, 263);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(160, 125);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "X";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button9.Location = new System.Drawing.Point(335, 263);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(161, 125);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "X";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 466);
+            this.Controls.Add(this.pnlField);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tlpButtons.ResumeLayout(false);
+            this.pnlField.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpButtons;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel pnlField;
     }
 }
 
